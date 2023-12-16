44acaf41fb005030e68fcd874cb517fbe9b26f2d
Zac <zacolden@gmail.com>
2023-12-16 22:31:59 +1100

    Add splash & home pages

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BotBuddy</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+<div class="min-h-screen grid grid-rows-[auto,1fr,auto] font-arial">
+    <div>
+        <div class="p-2">
+            <div class="mx-auto container text-lg font-bold">BotBuddy</div>
+        </div>
+    </div>
+    <div class="min-h-full">
+        <div class="p-2">
+            <div class="mx-auto container">Welcome</div>
+        </div>
+    </div>
+    <footer class="sticky top-[100vh]">
+        <div class="mx-auto container p-2">&copy; 2024 BotBuddy</div>
+    </footer>
+</div>
+</body>
+</html>
