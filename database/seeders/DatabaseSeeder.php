1028577700be41b16a9c009bbfae329b00734f9f
Zac <zacolden@gmail.com>
2024-01-04 16:18:02 +1100

    Demo account database seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,94 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::create([
+            'name' => 'demo',
+            'email' => 'demo@botbuddy.net',
+            'password' => Hash::make('demo'),
+            'email_verified_at' => now(),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $proxyGroup1 = $user->proxy_groups()->create([
+            'name' => 'Residential proxies',
+            'user_id' => $user->id,
+        ]);
+
+        $proxy1 = $proxyGroup1->proxies()->create([
+            'host' => 'atomicproxy.io',
+            'port' => 8080,
+            'password' => 'test123',
+            'user_id' => $user->id,
+        ]);
+
+        $proxy2 = $proxyGroup1->proxies()->create([
+            'host' => 'residential.datasource.net',
+            'port' => 25534,
+            'password' => '3D4uy$fb9',
+            'user_id' => $user->id,
+        ]);
+
+        $proxyGroup2 = $user->proxy_groups()->create([
+            'name' => 'Suicide proxies',
+            'user_id' => $user->id,
+        ]);
+
+        $proxy3 = $proxyGroup2->proxies()->create([
+            'host' => 'shitproxy.com',
+            'port' => 7777,
+            'password' => 'test123',
+            'user_id' => $user->id,
+        ]);
+
+        $script1 = $user->scripts()->create([
+            'name' => 'GE Profit Bot',
+            'user_id' => $user->id,
+            'script' => 'ge_profit',
+        ]);
+
+        $script2 = $user->scripts()->create([
+            'name' => 'Noob Cow Killer',
+            'user_id' => $user->id,
+            'script' => 'cowkiller',
+        ]);
+
+        $script3 = $user->scripts()->create([
+            'name' => 'Cooking Master 2.0',
+            'user_id' => $user->id,
+            'script' => 'cooking_master',
+        ]);
+
+        $accountGroup1 = $user->account_groups()->create([
+            'name' => '99 Cooking',
+            'user_id' => $user->id,
+        ]);
+
+        $account1 = $accountGroup1->accounts()->create([
+            'email' => 'cooking99@gmail.com',
+            'password' => 'test123',
+            'user_id' => $user->id,
+            'proxy_id' => $proxy1->id,
+            'script_id' => $script3->id,
+        ]);
+
+        $account2 = $accountGroup1->accounts()->create([
+            'email' => 'cooking.acc@hotmail.com',
+            'password' => 'mypassword23',
+            'user_id' => $user->id,
+            'proxy_id' => $proxy2->id,
+            'script_id' => $script3->id,
+        ]);
+
+        $accountGroup2 = $user->account_groups()->create([
+            'name' => 'Freshies',
+            'user_id' => $user->id,
+        ]);
+
+        $account3 = $accountGroup2->accounts()->create([
+            'email' => 'johnsmith5554@gmail.com',
+            'password' => 'lol123',
+            'user_id' => $user->id,
+            'proxy_id' => $proxy3->id,
+            'script_id' => $script2->id,
+        ]);
     }
 }
