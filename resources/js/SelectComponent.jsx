36479541a43196a0b3f2812c8917b3529bf62f1a
Zac <zacolden@gmail.com>
2024-01-13 12:16:56 +1100

    Refactor customizable select component

---
+++ resources/js/SelectComponent.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+const SelectComponent = ({data: {name, options, defaultValue, className}}) => {
+    const [selectedValue, setSelectedValue] = useState(defaultValue || options[0].value || options[0].label);
+    const handleSelectionChange = (event) => setSelectedValue(event.target.value);
+    const selectedOption = options.find(option => (option.value || option.label).toString() === selectedValue.toString());
+
+    return (
+        <>
+            <select name={name} className={className} onChange={handleSelectionChange}>
+                {options.map(option => (
+                    <option key={option.value} value={option.value} selected={selectedValue === option.value}>{option.label}</option>
+                ))}
+            </select>
+            {selectedOption && selectedOption.render && selectedOption.render()}
+        </>
+    );
+};
+
+export default SelectComponent;
