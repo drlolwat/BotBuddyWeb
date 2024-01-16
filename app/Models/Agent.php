1ea733a1f4600051a44715bd941e6f442bfc78c6
Zac <zacolden@gmail.com>
2024-01-16 17:02:05 +1100

    Add key columns to agent model

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -9,5 +9,5 @@ class Agent extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'user_id', 'uuid'];
+    protected $fillable = ['name', 'user_id', 'uuid', 'server_key', 'agent_key'];
 }
