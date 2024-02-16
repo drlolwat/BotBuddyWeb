70ce70506ccbb1c5c13b31e8efb1e02214f97aeb
Zac <zacolden@gmail.com>
2024-02-17 01:22:14 +1100

    Remove CSRF protection from store webhook

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -13,7 +13,7 @@ class StoreController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth', 'verified']);
+        $this->middleware(['auth', 'verified'])->except('webhook');
     }
 
     public function index()
