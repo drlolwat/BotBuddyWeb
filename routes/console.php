9f43691fa93740dcc0633a3425845ed5b1bfdc26
Zac <13795700+mink@users.noreply.github.com>
2024-03-23 00:00:01 +1100

    Laravel 11 upgrade

--- routes/console.php
+++ routes/console.php
@@ -1,19 +1,16 @@
 <?php
 
+use App\Jobs\CheckTempBannedAccounts;
+use App\Jobs\HeartbeatAccounts;
+use App\Jobs\StartQueuedAccounts;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::job(new HeartbeatAccounts())->everyMinute();
+Schedule::job(new StartQueuedAccounts())->everyMinute();
+Schedule::job(new CheckTempBannedAccounts())->everySixHours();
