3d90f69c860557414b2846d1396ef3182465dad2
Zac <13795700+mink@users.noreply.github.com>
2024-03-27 22:27:47 +1100

    Disable CSRF protection for Sellix webhook events again
    Forgot to add back after Laravel 11 upgrade

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
             remove: [\Illuminate\Routing\Middleware\ThrottleRequests::class.':api']
         );
 
+        $middleware->validateCsrfTokens(except: [
+            'store/webhook',
+        ]);
+
         $middleware->trustProxies(at: '*');
 
         $middleware->alias([
