29dea8564a8752ce0b8c5d33b3a98e00604881f7
lolwat <chchproud@gmail.com>
2024-04-03 07:35:52 -0400

    StartBotCommand.php: send disableBrowserProxy as string

--- app/BotBuddy/Socket/Commands/StartBotCommand.php
+++ app/BotBuddy/Socket/Commands/StartBotCommand.php
@@ -33,7 +33,7 @@ class StartBotCommand extends Command
             'accountTotp' => $this->account->password_2fa ?? '',
             'fps' => $this->account->fps,
             'world' => $this->account->world,
-            'disableBrowserProxy' => $this->account->account_group->disable_browser_proxy,
+            'disableBrowserProxy' => $this->account->account_group->disable_browser_proxy ? "true" : "false",
             'javaXmx' => $this->agent->dreambot_max_heap ?? '512M',
             'javaXms' => $this->agent->dreambot_min_heap ?? '256M',
         ];
