2880b1b0ba6282cd716b3e17950f86153374282b
Zac <13795700+mink@users.noreply.github.com>
2024-04-04 19:24:04 +1100

    Toggle schedule days off upon click

--- resources/js/components/MultiSelect.jsx
+++ resources/js/components/MultiSelect.jsx
@@ -32,9 +32,18 @@ const MultiSelect = () => {
             ))}
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Days</label>
             {selectedDays.length > 0 && <div
-                className="flex gap-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg rounded-b-none focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                className="w-full flex flex-wrap gap-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg rounded-b-none focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                 {selectedDays.map(day => (
-                    <div key={day} className="inline-flex bg-white text-black px-2 py-1 rounded-md">{day}</div>
+                    <div key={day} className="gap-2 bg-white text-black px-2 py-1 rounded-md inline-flex">
+                        <div>{day}</div>
+                        <div className="cursor-pointer" onClick={() => toggleDay(day)}>
+                            <svg className="w-3.5 mt-[.3rem] h-3.5" aria-hidden="true"
+                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"></path>
+                            </svg>
+                        </div>
+                    </div>
                 ))}
             </div>}
             <select
