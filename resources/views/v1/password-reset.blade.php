777503c4bdee4c7dcaa71d01484b3a364623d268
Zac <13795700+mink@users.noreply.github.com>
2024-04-05 21:44:00 +1100

    Fix reset password page

--- resources/views/v1/password-reset.blade.php
+++ resources/views/v1/password-reset.blade.php
@@ -35,8 +35,9 @@
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Change password
                     </h1>
-                    <form class="space-y-4 md:space-y-6" method="post">
+                    <form class="space-y-4 md:space-y-6" action="/reset-password" method="post">
                         @csrf
+                        <input type="hidden" name="token" value="{{ request()->route('token') }}">
                         <div>
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type your new password" required="">
