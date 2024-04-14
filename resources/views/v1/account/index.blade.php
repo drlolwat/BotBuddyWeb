527ca36f8b63294cbf88e447f5d818a2b23de93e
Zac <13795700+mink@users.noreply.github.com>
2024-04-14 19:12:28 +1000

    Add bulk script update action on accounts page

--- resources/views/v1/account/index.blade.php
+++ resources/views/v1/account/index.blade.php
@@ -18,6 +18,7 @@
                     <li><form id="bulk_export" method="post" action="{{ route('account.bulkAction') }}">@csrf<input type="hidden" name="action" value="export" /><button class="w-full text-left block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Export</button></form></li>
                     <li><button data-modal-target="default-modal" data-modal-toggle="default-modal" class="w-full text-left block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Queue start</button></li>
                     <li><button data-modal-target="change-agent-modal" data-modal-toggle="change-agent-modal" class="w-full text-left block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Change agent</button></li>
+                    <li><button data-modal-target="change-script-modal" data-modal-toggle="change-script-modal" class="w-full text-left block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Change script</button></li>
 {{--                    <li><a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Update proxy</a></li>--}}
 {{--                    <li><a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Update agent</a></li>--}}
                 </ul>
@@ -325,6 +326,40 @@
         </div>
     </div>
 
+    <div id="change-script-modal" tabindex="-1" aria-hidden="true" class="hidden bg-[#00000075] overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-full max-h-full">
+        <div class="relative p-4 w-full max-w-2xl max-h-full">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                        Change script
+                    </h3>
+                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="change-script-modal">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <form id="change_script" method="post" action="{{ route('account.bulkAction') }}">
+                    @csrf
+                    <input type="hidden" name="action" value="change_script" />
+                    <div class="p-4 md:p-5 space-y-4">
+                        <label for="script_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Script</label>
+                        <select name="script_id" id="script_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option value="0" selected>None</option>
+                            @foreach(auth()->user()->scripts as $script)
+                                <option value="{{ $script->id }}" >{{ $script->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                        <button type="submit" data-modal-hide="change-script-modal" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        <button data-modal-hide="change-script-modal" type="button" class="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
     <script>
         const modalToggleButton = document.querySelector('[data-modal-toggle="default-modal"]');
@@ -357,6 +392,21 @@
             closeButton.addEventListener('click', toggleChangeAgentModal);
         });
 
+        const changeScriptModalToggleButton = document.querySelector('[data-modal-toggle="change-script-modal"]');
+        const changeScriptModal = document.getElementById('change-script-modal');
+
+        function toggleChangeScriptModal() {
+            changeScriptModal.classList.toggle('hidden');
+            changeScriptModal.classList.toggle('flex');
+            changeScriptModal.setAttribute('aria-hidden', String(changeScriptModal.classList.contains('hidden')));
+        }
+
+        changeScriptModalToggleButton.addEventListener('click', toggleChangeScriptModal);
+
+        document.querySelectorAll('[data-modal-hide="change-script-modal"]').forEach(closeButton => {
+            closeButton.addEventListener('click', toggleChangeScriptModal);
+        });
+
         const masterCheckbox = document.getElementById('checkbox-all-search');
         if (masterCheckbox) {
             const rowCheckboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -372,7 +422,7 @@
         const checkboxes = document.querySelectorAll('input[type="checkbox"][name^="accounts["]');
         checkboxes.forEach(function(checkbox) {
             checkbox.addEventListener('change', function() {
-                const formIds = ['bulk_start', 'bulk_stop', 'bulk_queue', 'bulk_export', 'change_agent'];
+                const formIds = ['bulk_start', 'bulk_stop', 'bulk_queue', 'bulk_export', 'change_agent', 'change_script'];
 
                 formIds.forEach(function(formId) {
                     const form = document.getElementById(formId);
@@ -401,7 +451,7 @@
         const checkboxAll = document.getElementById('checkbox-all-search');
         checkboxAll.addEventListener('change', function() {
             const checkboxes = document.querySelectorAll('input[type="checkbox"][name^="accounts["]');
-            const formIds = ['bulk_start', 'bulk_stop', 'bulk_queue'];
+            const formIds = ['bulk_start', 'bulk_stop', 'bulk_queue', 'bulk_export', 'change_agent', 'change_script'];
 
             checkboxes.forEach(checkbox => {
                 formIds.forEach(formId => {
