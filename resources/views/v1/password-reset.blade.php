0d7b5180d6f3c489b98cd27313b21b2b01a5318b
Zac <13795700+mink@users.noreply.github.com>
2024-06-01 11:22:09 +1000

    Fix password reset form

--- resources/views/v1/password-reset.blade.php
+++ resources/views/v1/password-reset.blade.php
@@ -39,6 +39,10 @@
                         @csrf
                         <input type="hidden" name="token" value="{{ request()->route('token') }}">
                         <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type your email" required="">
+                        </div>
+                        <div>
                             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type your new password" required="">
                         </div>
