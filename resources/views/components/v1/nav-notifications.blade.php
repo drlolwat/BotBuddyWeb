31d9b26f09b63b38a218b1d6ffd22a8f406b6e58
Zac <13795700+mink@users.noreply.github.com>
2024-06-05 23:01:29 +1000

    Display unread notification count on top nav

--- resources/views/components/v1/nav-notifications.blade.php
+++ resources/views/components/v1/nav-notifications.blade.php
@@ -1,6 +1,12 @@
 <div class="relative inline-block text-left">
     <button type="button" data-dropdown-toggle="notification-dropdown" class="p-2 mr-1 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
         <span class="sr-only">View notifications</span>
+        @php($notification_count = auth()->user()->notifications()->whereNull('opened_at')->count())
+        @if($notification_count > 0)
+            <span class="absolute bg-blue-500 text-blue-100 px-2 py-1 text-[.6rem] font-bold rounded-full -top-1 -right-1">
+            {{ $notification_count > 9 ? "9+" : $notification_count }}
+        </span>
+        @endif
         <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path>
         </svg>
@@ -12,10 +18,10 @@
             Notifications
         </div>
         <div>
-            @if(auth()->user()->notifications()->whereNull('opened_at')->count() == 0)
+            @if($notification_count == 0)
             <div class="text-gray-500 font-normal text-sm dark:text-gray-400 flex py-3 px-4">You have no new notifications.</div>
             @endif
-                @if(auth()->user()->notifications()->whereNull('opened_at')->count() > 0)
+                @if($notification_count > 0)
                 @foreach(auth()->user()->notifications()->whereNull('opened_at')->orderByDesc('id')->limit(3)->get() as $notification)
             <a
                 href="#"
