08e1a52c85a7693b733930788800ac59a0c3d800
Zac <zacolden@gmail.com>
2024-11-23 11:57:13 +1100

    Update create/update form boxes

--- resources/views/v1/agent/create.blade.php
+++ resources/views/v1/agent/create.blade.php
@@ -1,39 +1,45 @@
 <x-v1.layout page="Add agent">
     <div class="max-w-2xl px-4 mx-auto">
-        <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add agent</h2>
-        <form method="post" action="{{ route('agent.store') }}">
-            @csrf
-            <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
-                <div class="sm:col-span-2">
-                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                    <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type the agent name">
-                </div>
-                <div class="sm:col-span-2">
-                    <label for="dreambot_client_path" class="block text-sm font-medium text-gray-900 dark:text-white">DreamBot client.jar location</label>
-                    <div class="mb-2 text-sm text-gray-900 dark:text-gray-400">e.g. C:\Users\User\DreamBot\BotData\client.jar</div>
-                    <input type="text" name="dreambot_client_path" id="dreambot_client_path" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type the path on your device to client.jar">
-                </div>
-                <div class="sm:col-span-2">
-                    <label for="dreambot_scripts_path" class="block text-sm font-medium text-gray-900 dark:text-white">DreamBot scripts location</label>
-                    <div class="mb-2 text-sm text-gray-900 dark:text-gray-400">e.g. C:\Users\User\DreamBot\Scripts</div>
-                    <input type="text" name="dreambot_scripts_path" id="dreambot_scripts_path" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type the path on your device to the scripts folder">
-                </div>
-                <div class="sm:col-span-2">
-                    <label for="dreambot_min_heap" class="block text-sm font-medium text-gray-900 dark:text-white">DreamBot minimum RAM</label>
-                    <div class="mb-2 text-sm text-gray-900 dark:text-gray-400">e.g. 256M or 1G</div>
-                    <input type="text" name="dreambot_min_heap" id="dreambot_min_heap" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="256M" placeholder="Type the min amount of RAM to allocate to DreamBot">
-                </div>
-                <div class="sm:col-span-2">
-                    <label for="dreambot_max_heap" class="block text-sm font-medium text-gray-900 dark:text-white">DreamBot maximum RAM</label>
-                    <div class="mb-2 text-sm text-gray-900 dark:text-gray-400">e.g. 512M or 4G</div>
-                    <input type="text" name="dreambot_max_heap" id="dreambot_max_heap" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="512M" placeholder="Type the max amount of RAM to allocate to DreamBot">
+        <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg">
+            <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+                <div class="w-full">
+                    <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add agent</h2>
+                    <form method="post" action="{{ route('agent.store') }}">
+                        @csrf
+                        <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
+                            <div class="sm:col-span-2">
+                                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                                <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type the agent name">
+                            </div>
+                            <div class="sm:col-span-2">
+                                <label for="dreambot_client_path" class="block text-sm font-medium text-gray-900 dark:text-white">DreamBot client.jar location</label>
+                                <div class="mb-2 text-sm text-gray-900 dark:text-gray-400">e.g. C:\Users\User\DreamBot\BotData\client.jar</div>
+                                <input type="text" name="dreambot_client_path" id="dreambot_client_path" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type the path on your device to client.jar">
+                            </div>
+                            <div class="sm:col-span-2">
+                                <label for="dreambot_scripts_path" class="block text-sm font-medium text-gray-900 dark:text-white">DreamBot scripts location</label>
+                                <div class="mb-2 text-sm text-gray-900 dark:text-gray-400">e.g. C:\Users\User\DreamBot\Scripts</div>
+                                <input type="text" name="dreambot_scripts_path" id="dreambot_scripts_path" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type the path on your device to the scripts folder">
+                            </div>
+                            <div class="sm:col-span-2">
+                                <label for="dreambot_min_heap" class="block text-sm font-medium text-gray-900 dark:text-white">DreamBot minimum RAM</label>
+                                <div class="mb-2 text-sm text-gray-900 dark:text-gray-400">e.g. 256M or 1G</div>
+                                <input type="text" name="dreambot_min_heap" id="dreambot_min_heap" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="256M" placeholder="Type the min amount of RAM to allocate to DreamBot">
+                            </div>
+                            <div class="sm:col-span-2">
+                                <label for="dreambot_max_heap" class="block text-sm font-medium text-gray-900 dark:text-white">DreamBot maximum RAM</label>
+                                <div class="mb-2 text-sm text-gray-900 dark:text-gray-400">e.g. 512M or 4G</div>
+                                <input type="text" name="dreambot_max_heap" id="dreambot_max_heap" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="512M" placeholder="Type the max amount of RAM to allocate to DreamBot">
+                            </div>
+                        </div>
+                        <div class="flex items-center space-x-4">
+                            <button type="submit" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                Add
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
-            <div class="flex items-center space-x-4">
-                <button type="submit" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                    Add
-                </button>
-            </div>
-        </form>
+        </div>
     </div>
 </x-v1.layout>
--- resources/views/v1/script/show.blade.php
+++ resources/views/v1/script/show.blade.php
@@ -1,32 +1,39 @@
 <x-v1.layout page="{{ $script->name }}">
     <div class="max-w-2xl px-4 mx-auto">
-        <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Update script</h2>
-        <form method="post" action="{{ route('script.update', $script->id) }}">
-            @csrf
-            <input type="hidden" name="_method" value="PUT">
-            <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
-                <div class="sm:col-span-2">
-                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                    <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="{{ $script->name }}" placeholder="Type the script name">
-                </div>
-                <div class="sm:col-span-2">
-                    <label for="script" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Script</label>
-                    <input type="text" name="script" id="script" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="{{ $script->script }}" placeholder="Type the name of the script on the client">
+        <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg">
+            <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+                <div class="w-full">
+                    <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Update script</h2>
+                    <form method="post" action="{{ route('script.update', $script->id) }}">
+                        @csrf
+                        <input type="hidden" name="_method" value="PUT">
+                        <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
+                            <div class="sm:col-span-2">
+                                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                                <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="{{ $script->name }}" placeholder="Type the script name">
+                            </div>
+                            <div class="sm:col-span-2">
+                                <label for="script" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Script</label>
+                                <input type="text" name="script" id="script" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="{{ $script->script }}" placeholder="Type the name of the script on the client">
+                            </div>
+                        </div>
+                        <div class="flex items-center space-x-4">
+                            <button type="submit" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                Update
+                            </button>
+                            <button form="delete_script" type="submit" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                                Delete
+                            </button>
+                        </div>
+                    </form>
+                    <form id="delete_script" method="post" action="{{ route('script.destroy', $script->id) }}">
+                        @csrf
+                        <input type="hidden" name="_method" value="DELETE">
+                    </form>
                 </div>
             </div>
-            <div class="flex items-center space-x-4">
-                <button type="submit" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                    Update
-                </button>
-                <button form="delete_script" type="submit" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-                    Delete
-                </button>
-            </div>
-        </form>
-        <form id="delete_script" method="post" action="{{ route('script.destroy', $script->id) }}">
-            @csrf
-            <input type="hidden" name="_method" value="DELETE">
-        </form>
+        </div>
+
     </div>
     <div class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4">
         <div>
@@ -49,7 +56,7 @@
             <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg">
                 <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                     <div class="w-full md:w-1/2">
-                        <div class="font-bold text-gray-900 dark:text-white">Script triggers</div>
+                        <div class="font-bold text-gray-900 dark:text-white">Script completion messages</div>
                     </div>
                     <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
                         <a href="{{ route('script.trigger.create', $script->id) }}" class="flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
