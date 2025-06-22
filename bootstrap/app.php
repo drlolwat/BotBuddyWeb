d9a056326fac4110a55605fb3ced0547c890afd5
Zac <zacolden@gmail.com>
2025-06-22 14:18:10 +1000

    Remove secret from webhook

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
         );
 
         $middleware->validateCsrfTokens(except: [
-            'store/webhook/dw89hfg389hg8fh89rg',
+            'store/webhook',
         ]);
 
         $middleware->trustProxies(at: '*');
--- routes/web.php
+++ routes/web.php
@@ -142,7 +142,7 @@ Route::group(['middleware' => ['verified', 'has.never.subscribed']], function() 
 
 Route::get('store', [\App\Http\Controllers\StoreController::class, 'index'])->name('store');
 Route::get('store/{product}', [\App\Http\Controllers\StoreController::class, 'checkout'])->name('store.checkout');
-Route::post('store/webhook/dw89hfg389hg8fh89rg', [\App\Http\Controllers\StoreController::class, 'webhook'])->name('store.webhook');
+Route::post('store/webhook', [\App\Http\Controllers\StoreController::class, 'webhook'])->name('store.webhook');
 
 Route::get('checkout/success', function () {
     return redirect(route('store'))->with('status', 'Thank you! We are processing your purchase.');
